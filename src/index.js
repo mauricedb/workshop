95051de7dbd0123ccf12601490af6a92728d9c9e
Maurice de Beijer <maurice.de.beijer@gmail.com>
2016-11-11 14:07:14 +0100

    Refactored file names to JSX

--- src/index.js
+++ src/index.js
@@ -3,5 +3,4 @@ import ReactDOM from 'react-dom';
 import App from './app-component';
 import '../node_modules/bootstrap/dist/css/bootstrap.min.css';
 
-ReactDOM.render(
-  <App />, document.getElementById('root'));
+ReactDOM.render(React.createElement(App), document.getElementById('root'));
