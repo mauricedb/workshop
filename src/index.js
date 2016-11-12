5e8bd32465955dd31e34fb1a57b9ef5ab5260f57
Maurice de Beijer <maurice.de.beijer@gmail.com>
2016-11-12 16:10:04 +0100

    Rename

--- src/index.js
+++ src/index.js
@@ -1,6 +1,6 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import App from './app-component';
+import App from './app-container';
 import '../node_modules/bootstrap/dist/css/bootstrap.min.css';
 
 ReactDOM.render(React.createElement(App), document.getElementById('root'));
